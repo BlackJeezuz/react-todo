97963549c40a13aee86ea19fb75eb363d1a38996
BlackJeezuz <ruslan201010@gmail.com>
2019-03-04 22:39:17 +0200

    Updates

---
+++ precache-manifest.220d62f8254890b8c2bd9dfe1723a3e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "39e1f1ecc2095f25e788",
+    "url": "/react-todo/static/js/main.39e1f1ec.chunk.js"
+  },
+  {
+    "revision": "b2e928c5dfcb945f1d25",
+    "url": "/react-todo/static/js/2.b2e928c5.chunk.js"
+  },
+  {
+    "revision": "39e1f1ecc2095f25e788",
+    "url": "/react-todo/static/css/main.8ddd0262.chunk.css"
+  },
+  {
+    "revision": "6215938be6fe166cabc6b9abe8b7b175",
+    "url": "/react-todo/index.html"
+  }
+];
